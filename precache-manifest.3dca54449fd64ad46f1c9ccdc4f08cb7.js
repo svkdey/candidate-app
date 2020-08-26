228148603ca7bbbc85068f2aad0aa4b7039b144f
svkdey <svkdey9@gmail.com>
2020-08-26 21:45:32 +0530

    Updates

---
+++ precache-manifest.3dca54449fd64ad46f1c9ccdc4f08cb7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bde02a555aed0ed667917032fe790e9",
+    "url": "/candidate-app/index.html"
+  },
+  {
+    "revision": "331df38595bfab3e2540",
+    "url": "/candidate-app/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "fa8d5df4c3487b4c2357",
+    "url": "/candidate-app/static/css/main.c4c7769b.chunk.css"
+  },
+  {
+    "revision": "331df38595bfab3e2540",
+    "url": "/candidate-app/static/js/2.12718c00.chunk.js"
+  },
+  {
+    "revision": "3e7812a7c01a0a82e0947874d754ae52",
+    "url": "/candidate-app/static/js/2.12718c00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa8d5df4c3487b4c2357",
+    "url": "/candidate-app/static/js/main.99b5d3dd.chunk.js"
+  },
+  {
+    "revision": "4f6ab0973ba380a4f23e",
+    "url": "/candidate-app/static/js/runtime-main.16dbcce2.js"
+  }
+]);
